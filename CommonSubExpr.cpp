fb8458fd7fdf7c91328155f0ee84fc72410731e1
Kyle Yang <REmpire.dev@proton.me>
2025-08-07 20:11:57 -0600

    Implemented common sub expresion pass and constant folding pass

--- CommonSubExpr.cpp
+++ CommonSubExpr.cpp
@@ -1,8 +1,12 @@
 #include "llvm/IR/CFG.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/Instructions.h"
 #include "llvm/IR/LegacyPassManager.h"
 #include "llvm/Pass.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
+
+#include <unordered_set>
 
 using namespace llvm;
 
@@ -11,6 +15,36 @@ namespace {
         static char ID;
         CommonSubExprPass() : FunctionPass(ID) {}
 
+        std::unordered_set<Instruction*> ToDelete;
+        
+        virtual bool runOnFunction(Function &F) {
+            errs() << "Running CommonSubExpr on function: " << F.getName() << "!\n";
+            do {
+                ToDelete.clear();
+                for (auto &basicBlock : F) {
+                    for (auto it1 = basicBlock.begin(); it1 != basicBlock.end(); ++it1) {
+                        Instruction *I1 = &(*it1);
+                        if (isa<AllocaInst>(I1)) continue;
+
+                        llvm::Value *loadOp = isa<LoadInst>(I1) ? cast<LoadInst>(I1)->getPointerOperand() : nullptr;
+
+                        for (auto it2 = std::next(it1); it2 != basicBlock.end(); ++it2) {
+                            Instruction *I2 = &(*it2);
+                            auto *storeInst = dyn_cast<StoreInst>(I2);
+                            if (storeInst && storeInst->getPointerOperand() == loadOp) break;
+
+                            if (I1 != I2 && !ToDelete.count(I1) && !ToDelete.count(I2) && I1->isIdenticalTo(I2)) {
+                                ToDelete.emplace(I2);
+                                I2->replaceAllUsesWith(I1);
+                            }
+                        }
+                    }
+                }
+                for (Instruction *inst : ToDelete) inst->eraseFromParent();
+            } while (!ToDelete.empty());
+            return true;
+
+        }
     };
 }
 
@@ -18,6 +52,8 @@ char CommonSubExprPass::ID = 2;
 
 static RegisterPass<CommonSubExprPass> X("CSE", "Common Sub-expression Elimination pass for minic", false, false);
 
+// Automatically enable the pass.
+// http://adriansampson.net/blog/clangpass.html
 static void registerCommonSubExprPass(const PassManagerBuilder &, legacy::PassManagerBase &PM) {
      PM.add(new CommonSubExprPass()); 
 }
--- ConstantFolding.cpp
+++ ConstantFolding.cpp
@@ -1,6 +1,10 @@
 #include "llvm/Pass.h"
+#include "llvm/IR/Function.h"
 #include "llvm/IR/LegacyPassManager.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/ConstantFold.h"
+#include "llvm/IR/Instructions.h"
 
 using namespace llvm;
 
@@ -10,7 +14,47 @@ namespace {
         ConstantFolding() : FunctionPass(ID) {}
 
         virtual bool runOnFunction(Function &F) {
+            errs() << "Constant Folding Working on function: " << F.getName() << "!\n";
 
+            bool changed = false;
+            std::vector<Instruction*> toDelete;
+
+            for (auto &basicBlock : F) {
+                for (auto &inst : basicBlock) {
+                    if (isa<BinaryOperator>(&inst) &&
+                        isa<ConstantInt>(inst.getOperand(0)) && 
+                        isa<ConstantInt>(inst.getOperand(1))) {
+
+                        auto *ins = cast<BinaryOperator>(&inst);
+                        auto *op1 = cast<ConstantInt>(ins->getOperand(0));
+                        auto *op2 = cast<ConstantInt>(ins->getOperand(1));
+
+                        if (auto *res = ConstantFoldBinaryInstruction(ins->getOpcode(), op1, op2)) {
+                                inst.replaceAllUsesWith(res);
+                                toDelete.emplace_back(&inst);
+                                changed = true;
+                            }
+                        }
+                    else if (isa<ICmpInst>(&inst) &&
+                             isa<ConstantInt>(inst.getOperand(0)) && 
+                             isa<ConstantInt>(inst.getOperand(1))) {
+
+                        auto *ins = cast<ICmpInst>(&inst);
+                        auto *op1 = cast<ConstantInt>(ins->getOperand(0));
+                        auto *op2 = cast<ConstantInt>(ins->getOperand(1));
+
+                        if (auto *res = ConstantFoldCompareInstruction(ins->getPredicate(), op1, op2)) {
+                            inst.replaceAllUsesWith(res);
+                            toDelete.emplace_back(&inst);
+                            changed = true;
+                        }
+                    }
+                }
+            }
+
+            for (auto *inst : toDelete) inst->eraseFromParent();
+            return changed;
+        }
     };
 }
 
@@ -18,6 +62,8 @@ char ConstantFolding::ID = 3;
 
 static RegisterPass<ConstantFolding> X("const-fold", "Constant-Folding pass for minic", false, false);
 
+// Automatically enable the pass.
+// http://adriansampson.net/blog/clangpass.html
 static void registerConstantFoldingPass(const PassManagerBuilder &,
                                     legacy::PassManagerBase &PM) {
     PM.add(new ConstantFolding());
